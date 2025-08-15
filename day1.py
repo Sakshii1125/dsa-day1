9106869ad2dbcb48399f7823e358ed5081ec1f74
Sakshi Jadhav <139758810+Sakshii1125@users.noreply.github.com>
2025-08-15 22:27:04 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,30 @@
+def sort012(arr):
+    low = 0              
+    mid = 0              
+    high = len(arr) - 1  
+
+
+    while mid <= high:
+        if arr[mid] == 0:
+           
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+
+            mid += 1
+        else:
+            
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1 
+
+    return arr
+
+
+# Test cases
+print(sort012([0, 1, 2, 1, 0, 2, 1, 0])) 
+print(sort012([2, 2, 2, 2]))             
+print(sort012([0, 0, 0, 0]))
+print(sort012([1, 1, 1, 1]))              
+print(sort012([2, 0, 1]))               
+print(sort012([]))
