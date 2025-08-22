c8d7aa19fa8c4a36f01ace63df71d05463f2462f
Sakshi Jadhav <139758810+Sakshii1125@users.noreply.github.com>
2025-08-22 22:48:14 +0530

    Create day-8.py
    Reverse a String Word by Word

---
+++ day-8.py
@@ -0,0 +1,14 @@
+def reverse_words(s: str) -> str:
+
+    words = s.split()
+    
+    words.reverse()
+    
+    return " ".join(words)
+    
+    
+print(reverse_words("the sky is blue"))      
+print(reverse_words("  hello world  "))       
+print(reverse_words("a good   example"))      
+print(reverse_words("    "))                  
+print(reverse_words("word"))    
